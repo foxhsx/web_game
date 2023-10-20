ca74163bf20e69c589a898ad831965ba7787d6e5
foxhsx <932163475@qq.com>
2023-10-20 23:58:57 +0800

    feat: 给小恐龙增加闪烁和眨眼睛的动画

--- react-tetris/src/components/Logo/index.jsx
+++ react-tetris/src/components/Logo/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { i18n, lan } from '../../unit/const'
 import style from './index.module.scss'
 
 const Logo = () => {
+  const [blink, setBlink] = useState('')
+
+  let count = 0;
+  const eyes = useCallback((eyesState, delay) => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    count += 1;
+    setTimeout(() => {
+      setBlink(eyesState)
+      if (count < 8) {
+        eyes(eyesState === 'eyes' ? '' : 'eyes', 150)
+      }
+    }, delay)
+  }, [])
+
+  useEffect(() => {
+    eyes('eyes', 1500)
+  }, [eyes])
+
   return (
     <div className={style.logo}>
-      <div className='bg dragon'></div>
+      <div className={`bg dragon ${blink}`}></div>
       <p dangerouslySetInnerHTML={{ __html: i18n.titleCenter[lan] }}></p>
     </div>
   )
