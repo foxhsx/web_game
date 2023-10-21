d78f87d52788b83ce9ca37befd6ec5f36b056708
foxhsx <932163475@qq.com>
2023-10-22 00:46:42 +0800

    feat: 增加恐龙跑步的动画，但是有问题

--- react-tetris/src/components/Logo/index.jsx
+++ react-tetris/src/components/Logo/index.jsx
@@ -2,20 +2,46 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { i18n, lan } from '../../unit/const'
 import style from './index.module.scss'
 
-const Logo = () => {
-  const [blink, setBlink] = useState('')
+let count = 0;
+let direction = 'r';
+let position = '4';
 
-  let count = 0;
-  const eyes = useCallback((eyesState, delay) => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    count += 1;
+const Logo = () => {
+  const [className, setClassName] = useState('')
+
+  const run = useCallback((name) => {
+    count++
     setTimeout(() => {
-      setBlink(eyesState)
+      if (count < 49) {
+        setClassName(name)
+        // ! 动画有问题，刚刷新的时候就不行
+        if ([19, 29, 39].includes(count)) {
+          direction = direction === 'r' ? 'l' : 'r'
+        }
+        position = position === '4' ? '3' : '4'
+        run(direction+position)
+      } else {
+        // setClassName(`${direction}1`)
+        // count = 0
+        // position = '4';
+        // setTimeout(() => {
+        //   eyes('eyes', 1500)
+        // }, 4000)
+      }
+    }, 100)
+  }, [])
+
+  const eyes = useCallback((eyesState, delay) => {
+    count++;
+    setTimeout(() => {
+      setClassName(eyesState)
       if (count < 8) {
         eyes(eyesState === 'eyes' ? '' : 'eyes', 150)
+      } else {
+        run(direction+position)
       }
     }, delay)
-  }, [])
+  }, [run])
 
   useEffect(() => {
     eyes('eyes', 1500)
@@ -23,7 +49,7 @@ const Logo = () => {
 
   return (
     <div className={style.logo}>
-      <div className={`bg dragon ${blink}`}></div>
+      <div className={`bg dragon ${className}`}></div>
       <p dangerouslySetInnerHTML={{ __html: i18n.titleCenter[lan] }}></p>
     </div>
   )
